5ca5e23bc35545c3dcb2c5ea68540433b59f2e43
dantheman <danogbo0@gmail.com>
2023-06-28 12:34:05 +0100

    error handling commit

--- src/Index.java
+++ src/Index.java
@@ -15,6 +15,21 @@ public class Index {
         System.out.println("c) 2");
         System.out.println("d) 1");
 
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                String userInput = input.next();
+
+                if (!userInput.matches("[abcd]")) {
+                    throw new IllegalArgumentException("Invalid input. Only a, b, c, or d are allowed.");
+                }
+                responses[0] = userInput;
+                validInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("Please enter a valid option (a, b, c, or d):");
+            }
+        }
 
         System.out.println("Who is the current president of Nigeria?");
         System.out.println("a) Daniel");
@@ -22,6 +37,21 @@ public class Index {
         System.out.println("c) Balablu");
         System.out.println("d) Tinubu");
 
+        boolean validInput1 = false;
+        while (!validInput1) {
+            try {
+                String userInput = input.next();
+
+                if (!userInput.matches("[abcd]")) {
+                    throw new IllegalArgumentException("Invalid input. Only a, b, c, or d are allowed.");
+                }
+                responses[1] = userInput;
+                validInput1 = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("Please enter a valid option (a, b, c, or d):");
+            }
+        }
 
         System.out.println("How many states are in Nigeria?");
         System.out.println("a) 40");
@@ -29,9 +59,21 @@ public class Index {
         System.out.println("c) 26");
         System.out.println("d) 19");
 
-        responses[0] = input.next();
-        responses[1] = input.next();
-        responses[2] = input.next();
+        boolean validInput2 = false;
+        while (!validInput2) {
+            try {
+                String userInput = input.next();
+
+                if (!userInput.matches("[abcd]")) {
+                    throw new IllegalArgumentException("invalid input . only 'a', 'b', 'c', or 'd' are allowed.");
+                }
+                responses[2] = userInput;
+                validInput2 = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("error: " + e.getMessage());
+                System.out.println("please enter a valid option (a, b, c, or d):");
+            }
+        }
 
         int scores = 0;
         for (int i = 0; i < 3; i++) {
